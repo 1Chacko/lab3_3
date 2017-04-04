e9242a7b20122ae7d728310d81043c83c89331f5
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-04-04 15:58:58 +0200

    Refactor klasy OrderTest

--- src/main/java/edu/iis/mto/test/OrderTest.java
+++ src/main/java/edu/iis/mto/test/OrderTest.java
@@ -5,14 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import edu.iis.mto.time.Order;
+import edu.iis.mto.time.OrderExpiredException;
 
 public class OrderTest {
 
 	@Test
 	public void testThrowOfOrderExpiredException() {
+		
 		Order order = new Order();
 		order.submit();
 		order.confirm();
+		
 	}
 
 }
