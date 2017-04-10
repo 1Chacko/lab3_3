80ce13b266717d4500b6618d5f7b81931a23283f
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-04-10 21:39:27 +0200

    Dodanie testThrowOfOrderStateException

--- src/main/java/edu/iis/mto/test/OrderTest.java
+++ src/main/java/edu/iis/mto/test/OrderTest.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import edu.iis.mto.fakeclock.FakeClock;
+import edu.iis.mto.fakeclock.RealClock;
 import edu.iis.mto.time.Order;
 import edu.iis.mto.time.OrderExpiredException;
+import edu.iis.mto.time.OrderStateException;
 
 public class OrderTest {
 
@@ -18,5 +20,13 @@ public class OrderTest {
 		order.confirm(new FakeClock());
 		
 	}
-
+	
+	@Test(expected = OrderStateException.class)
+	public void testThrowOfOrderStateException() {
+		
+		Order order = new Order();
+		order.confirm(new RealClock());
+		
+	}
+	
 }
